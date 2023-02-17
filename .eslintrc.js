397e9a17714b7998899e1292ff7de1663652cf2a
Ian Yong <ianyongyc@gmail.com>
2023-02-17 14:33:55 +0800

    Set up & configure Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,15 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  extends: [
+    'standard-with-typescript',
+    // Prettier should always come last so that it overrides other configs.
+    'prettier'
   ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  rules: {
-  }
-}
+  rules: {}
+};
