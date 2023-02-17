bf96505e6c6b82c33d78b1961bd94de766f4e79c
Ian Yong <ianyongyc@gmail.com>
2023-02-17 15:05:30 +0800

    Make ESLint work with TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: 'tsconfig.json'
   },
   rules: {}
 };
