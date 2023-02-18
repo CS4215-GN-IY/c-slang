4a7c624ea34a17d419fcb041dcd0a9c70601ec4f
Geng Ning <gengninghan1@gmail.com>
2023-02-18 21:51:37 +0800

    Add parser

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -1,6 +1,17 @@
+import { start } from 'repl';
+import { parse } from '../parser/parser';
+
+const startRepl = (): void => {
+  start({
+    eval: (text, unusedContext, unusedFileName, callback) => {
+      const tree: string = parse(text);
+      callback(null, tree);
+    }
+  });
+};
+
 const main = (): void => {
-  // TODO: Implement REPL.
-  console.log('Hello!');
+  startRepl();
 };
 
 main();
