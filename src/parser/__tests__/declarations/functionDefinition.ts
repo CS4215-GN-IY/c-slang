38272e9180f5839e4051a9df14ef225b5a393eff
Ian Yong <ianyongyc@gmail.com>
2023-03-29 02:42:15 +0800

    Add function definition tests

---
+++ src/parser/__tests__/declarations/functionDefinition.ts
@@ -0,0 +1,191 @@
+import { parse } from '../../parser';
+import { type Program } from '../../../ast/types';
+
+describe('function definition', () => {
+  test('handles function definitions without parameters', () => {
+    const code = 'int main() { return 3 + 7; }';
+    const ast = parse(code);
+    const expectedAst: Program = {
+      type: 'Program',
+      body: [
+        {
+          type: 'FunctionDeclaration',
+          id: {
+            type: 'Identifier',
+            name: 'main'
+          },
+          params: [],
+          body: {
+            type: 'BlockStatement',
+            items: [
+              {
+                type: 'ReturnStatement',
+                argument: {
+                  type: 'SequenceExpression',
+                  expressions: [
+                    {
+                      type: 'BinaryExpression',
+                      operator: '+',
+                      left: {
+                        type: 'Constant',
+                        value: 3
+                      },
+                      right: {
+                        type: 'Constant',
+                        value: 7
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    };
+    expect(ast).toEqual(expectedAst);
+  });
+
+  test('handles function definitions with parameters', () => {
+    const code = 'int sum(int a, int b) { return a + b; }';
+    const ast = parse(code);
+    const expectedAst: Program = {
+      type: 'Program',
+      body: [
+        {
+          type: 'FunctionDeclaration',
+          id: {
+            type: 'Identifier',
+            name: 'sum'
+          },
+          params: [
+            {
+              type: 'Identifier',
+              name: 'a'
+            },
+            {
+              type: 'Identifier',
+              name: 'b'
+            }
+          ],
+          body: {
+            type: 'BlockStatement',
+            items: [
+              {
+                type: 'ReturnStatement',
+                argument: {
+                  type: 'SequenceExpression',
+                  expressions: [
+                    {
+                      type: 'BinaryExpression',
+                      operator: '+',
+                      left: {
+                        type: 'Identifier',
+                        name: 'a'
+                      },
+                      right: {
+                        type: 'Identifier',
+                        name: 'b'
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    };
+    expect(ast).toEqual(expectedAst);
+  });
+
+  test('handles function definitions which return a function pointer', () => {
+    const code = 'int (*g(int a, int b))() { return f; }';
+    const ast = parse(code);
+    const expectedAst: Program = {
+      type: 'Program',
+      body: [
+        {
+          type: 'FunctionDeclaration',
+          id: {
+            type: 'Identifier',
+            name: 'g'
+          },
+          params: [
+            {
+              type: 'Identifier',
+              name: 'a'
+            },
+            {
+              type: 'Identifier',
+              name: 'b'
+            }
+          ],
+          body: {
+            type: 'BlockStatement',
+            items: [
+              {
+                type: 'ReturnStatement',
+                argument: {
+                  type: 'SequenceExpression',
+                  expressions: [
+                    {
+                      type: 'Identifier',
+                      name: 'f'
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    };
+    expect(ast).toEqual(expectedAst);
+  });
+
+  test('handles function definitions which return a function which returns a function pointer', () => {
+    const code = 'int (*(*h(int a, int b))())() { return g; }';
+    const ast = parse(code);
+    const expectedAst: Program = {
+      type: 'Program',
+      body: [
+        {
+          type: 'FunctionDeclaration',
+          id: {
+            type: 'Identifier',
+            name: 'h'
+          },
+          params: [
+            {
+              type: 'Identifier',
+              name: 'a'
+            },
+            {
+              type: 'Identifier',
+              name: 'b'
+            }
+          ],
+          body: {
+            type: 'BlockStatement',
+            items: [
+              {
+                type: 'ReturnStatement',
+                argument: {
+                  type: 'SequenceExpression',
+                  expressions: [
+                    {
+                      type: 'Identifier',
+                      name: 'g'
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    };
+    expect(ast).toEqual(expectedAst);
+  });
+});
