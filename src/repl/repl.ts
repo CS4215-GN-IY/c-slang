256c706923fcf7115e0e5b9bd147abc98cf7781d
Ian Yong <ianyongyc@gmail.com>
2023-04-08 12:56:18 +0800

    Fix REPL

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -9,16 +9,15 @@ const startRepl = (): void => {
       replInput: string,
       _context: Context,
       _fileName: string,
-      callback: (err: Error | null, result: any) => void
+      callback: (err: Error | null, result: Value) => void
     ) => {
-      void run(replInput).then((result: Value) => {
-        if (result.status === 'error') {
-          // TODO: Implement error handling.
-          callback(new Error('An error occurred!'), undefined);
-          return;
-        }
-        callback(null, result.value);
-      });
+      void run(replInput)
+        .then((result: Value) => {
+          callback(null, result);
+        })
+        .catch((err) => {
+          callback(err, undefined);
+        });
     }
   });
 };
