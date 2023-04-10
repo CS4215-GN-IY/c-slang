985ff7b65679529f3832d42b480484387cdaf594
Ian Yong <ianyongyc@gmail.com>
2023-04-10 22:19:27 +0800

    Convert data type types into constants

--- src/ast/types/dataTypes.ts
+++ src/ast/types/dataTypes.ts
@@ -1,58 +1,70 @@
+export type DataType = IntegerDataType | FloatingPointDataType;
+
 export interface IntegerDataType {
   type: 'Integer';
   sizeInBytes: number;
   isSigned: boolean;
 }
 
-export interface Int8 extends IntegerDataType {
-  sizeInBytes: 1;
-  isSigned: true;
-}
+export const INT8: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 1,
+  isSigned: true
+};
 
-export interface UInt8 extends IntegerDataType {
-  sizeInBytes: 1;
-  isSigned: false;
-}
+export const UINT8: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 1,
+  isSigned: false
+};
 
-export interface Int16 extends IntegerDataType {
-  sizeInBytes: 2;
-  isSigned: true;
-}
+export const INT16: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 2,
+  isSigned: true
+};
 
-export interface UInt16 extends IntegerDataType {
-  sizeInBytes: 2;
-  isSigned: false;
-}
+export const UINT16: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 2,
+  isSigned: false
+};
 
-export interface Int32 extends IntegerDataType {
-  sizeInBytes: 4;
-  isSigned: true;
-}
+export const INT32: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 4,
+  isSigned: true
+};
 
-export interface UInt32 extends IntegerDataType {
-  sizeInBytes: 4;
-  isSigned: false;
-}
+export const UINT32: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 4,
+  isSigned: false
+};
 
-export interface Int64 extends IntegerDataType {
-  sizeInBytes: 8;
-  isSigned: true;
-}
+export const INT64: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 8,
+  isSigned: true
+};
 
-export interface UInt64 extends IntegerDataType {
-  sizeInBytes: 8;
-  isSigned: false;
-}
+export const UINT64: IntegerDataType = {
+  type: 'Integer',
+  sizeInBytes: 8,
+  isSigned: false
+};
 
 export interface FloatingPointDataType {
   type: 'FloatingPoint';
   sizeInBytes: number;
 }
 
-export interface Float32 extends FloatingPointDataType {
-  sizeInBytes: 4;
-}
+export const FLOAT32: FloatingPointDataType = {
+  type: 'FloatingPoint',
+  sizeInBytes: 4
+};
 
-export interface Float64 extends FloatingPointDataType {
-  sizeInBytes: 8;
-}
+export const FLOAT64: FloatingPointDataType = {
+  type: 'FloatingPoint',
+  sizeInBytes: 8
+};
