99f7009394955cb918eb7835dde7a3ca9e1363de
Ian Yong <ianyongyc@gmail.com>
2023-04-14 10:18:52 +0800

    Add malloc & free

--- src/interpreter/builtins.ts
+++ src/interpreter/builtins.ts
@@ -6,12 +6,28 @@ export const getBuiltInFunctions = (
   memory: VirtualMemory
 ): Record<string, (...args: any[]) => any> => {
   return {
+    free: (address: number) => {
+      memory.heapFree(address);
+    },
+    malloc: (sizeInBytes: number) => memory.heapAllocate(sizeInBytes),
     sqrt: (x: number) => Math.sqrt(x)
   };
 };
 
 export const getBuiltInSymbols = (): SymbolTableFrame => {
   return {
+    free: {
+      nameType: 'BuiltInFunction',
+      name: 'free',
+      numOfParams: 1,
+      dataType: BUILTIN
+    },
+    malloc: {
+      nameType: 'BuiltInFunction',
+      name: 'malloc',
+      numOfParams: 1,
+      dataType: BUILTIN
+    },
     sqrt: {
       nameType: 'BuiltInFunction',
       name: 'sqrt',
